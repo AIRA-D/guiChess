98c28261e1e76c53032b1aecc0a5b326da112e4b
AIRA-D <aira_d@vk.com>
2024-10-21 19:16:34 +0300

    Update piece.cpp
    Реализация всех методов, объявленных в piece.h

--- source/piece.cpp
+++ source/piece.cpp
@@ -1,3 +1,52 @@
-#include "piece.h"
+#ifndef PIECE_H
+#define PIECE_H
 
-Piece::Piece() {}
+#include <QIcon>
+
+enum PieceType {
+    ROOK,
+    KNIGHT,
+    BISHOP,
+    QUEEN,
+    KING,
+    PAWN
+};
+
+enum PColor {
+    WHITE_P,
+    BLACK_P
+};
+
+struct Position {
+    int row;
+    int column;
+};
+
+
+class Piece
+{
+public:
+    static Piece* createPiece(PieceType type, PColor color, Position currentPiecePosition);
+
+    virtual QIcon getIcon() const = 0;
+    PColor getColor();
+    Position getPosition();
+
+    virtual bool isValidMove(const Position& from, const Position& to) const = 0;
+    virtual ~Piece();
+
+protected:
+    Piece(PieceType type, PColor color, Position currentPiecePosition) : type(type), color(color), currentPiecePosition(currentPiecePosition) {
+        initIcon();
+    };
+
+    PieceType type;
+    PColor color;
+    Position currentPiecePosition;
+    QIcon icon;
+
+private:
+    void initIcon();
+};
+
+#endif // PIECE_H

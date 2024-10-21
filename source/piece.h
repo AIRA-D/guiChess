0d87c6eb09d46ef7e414d1d69a24d9633614029c
AIRA-D <aira_d@vk.com>
2024-10-21 19:14:54 +0300

    Реорганизация + описание фигуры через enum
    Добавлены: PieceType, PColor, Position(удалено из board.h)

--- source/piece.h
+++ source/piece.h
@@ -1,15 +1,52 @@
 #ifndef PIECE_H
 #define PIECE_H
 
+#include <QIcon>
+
+enum PieceType {
+    ROOK,
+    KNIGHT,
+    BISHOP,
+    QUEEN,
+    KING,
+    PAWN
+};
+
+enum PColor {
+    WHITE_P,
+    BLACK_P
+};
+
+struct Position {
+    int row;
+    int column;
+};
+
+
 class Piece
 {
 public:
-    Piece();
-    ~Piece();
+    static Piece* createPiece(PieceType type, PColor color, Position currentPiecePosition);
+
+    virtual QIcon getIcon() const = 0;
+    PColor getColor();
+    Position getPosition();
+
+    virtual bool isValidMove(const Position& from, const Position& to) const = 0;
+    virtual ~Piece();
+
+protected:
+    Piece(PieceType type, PColor color, Position currentPiecePosition) : type(type), color(color), currentPiecePosition(currentPiecePosition) {
+        initIcon();
+    };
+
+    PieceType type;
+    PColor color;
+    Position currentPiecePosition;
+    QIcon icon;
 
 private:
-    void getPiecePosition();
-    void setPiecePosition();
+    void initIcon();
 };
 
 #endif // PIECE_H
